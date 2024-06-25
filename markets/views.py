15aa77834c3a9ff700996d481c9c8ea09162b2ef
Sarolta <115449000+saroltah@users.noreply.github.com>
2024-06-25 09:19:59 +0200

    add post method to records and markets

--- markets/views.py
+++ markets/views.py
@@ -4,15 +4,29 @@ from rest_framework.response import Response
 from .models import Market
 from .serializers import MarketSerializer
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
 from we_love_vinyls.permissions import IsOrganizerOrReadOnly
 
 
 class AllMarkets(APIView):
+
+    serializer_class = MarketSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
     def get(self, request):
         markets = Market.objects.all()
         serializer = MarketSerializer(markets, many= True, context = {'request' : request})
         return Response(serializer.data)
+
+    def post (self, request, pk):
+        market =self.get_object(pk)
+        serializer=MarketSerializer(market, data=request.data, context = {'request' : request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class OneMarket(APIView):
     serializer_class = MarketSerializer
--- records/views.py
+++ records/views.py
@@ -4,14 +4,29 @@ from rest_framework.response import Response
 from .models import Record
 from .serializers import RecordSerializer
 from django.http import Http404
+from rest_framework import status, permissions
 from we_love_vinyls.permissions import IsAdvertiserOrReadOnly
 
 
 class AllRecords(APIView):
+
+    serializer_class = RecordSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
     def get(self, request):
         records = Record.objects.all()
         serializer = RecordSerializer(records, many= True, context = {'request' : request})
         return Response(serializer.data)
+
+    def post (self, request, pk):
+        record =self.get_object(pk)
+        serializer=RecordSerializer(record, data=request.data, context = {'request' : request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class OneRecord(APIView):
     serializer_class = RecordSerializer
