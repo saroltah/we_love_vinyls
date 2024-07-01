761f8cd468631c0239cb5fbca6009ad4bd1367e0
Sarolta <115449000+saroltah@users.noreply.github.com>
2024-07-01 18:39:24 +0200

    prepare for registration
    temporarly disable rest-auth

--- we_love_vinyls/settings.py
+++ we_love_vinyls/settings.py
@@ -56,8 +56,10 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
 'DEFAULT_AUTHENTICATION_CLASSES': [( 
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication' 
+        if 'DEV' in os.environ 
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+
     )],
 'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
 'PAGE_SIZE': 15,
