d839b88b26a722e98316ec6506c7c9b1797b50f2
Sarolta Hegyi <sarolta.hegyi@gmail.com>
2024-07-21 18:13:35 +0000

    correct settings

--- we_love_vinyls/settings.py
+++ we_love_vinyls/settings.py
@@ -65,12 +65,22 @@ REST_FRAMEWORK = {
         )],
 
     'DATETIME_FORMAT': '%d %b %Y',
-    }
+
+  }
+
+if 'DEV' not in os.environ:
+        REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+            'rest_framework.renderers.JSONRenderer',
+        ]
+  
+
+    
 
 REST_USE_JWT = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 ACCOUNT_LOGOUT_ON_GET = False
 
