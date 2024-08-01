2913fe7b60094df2522d4c74bc36838825b90c04
Sarolta Hegyi <sarolta.hegyi@gmail.com>
2024-08-01 11:54:18 +0000

    modify like url

--- likes/urls.py
+++ likes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from likes import views
 
 urlpatterns = [
-    path('/likes/', views.AllLikes.as_view()),
-    path('/likes/<int:pk>/', views.OneLike.as_view()),
+    path('likes/', views.AllLikes.as_view()),
+    path('likes/<int:pk>/', views.OneLike.as_view()),
     path('attendance/', views.AllAttendance .as_view()),
     path('attendance/<int:pk>/', views.OneAttendance.as_view()),
 ]
