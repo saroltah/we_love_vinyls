ac65cd9829af23f12dbdad3d013dbc69d2ec0abb
Sarolta Hegyi <sarolta.hegyi@gmail.com>
2024-08-08 15:06:20 +0000

    remove pagination

--- we_love_vinyls/settings.py
+++ we_love_vinyls/settings.py
@@ -60,8 +60,7 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     
-    'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    
 
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
