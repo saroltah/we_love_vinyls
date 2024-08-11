fcbc39f9ca173646c518e7f7dd6cb8f435fd8134
Sarolta Hegyi <sarolta.hegyi@gmail.com>
2024-08-11 08:17:15 +0000

    import refresh cookie

--- we_love_vinyls/views.py
+++ we_love_vinyls/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_SAMESITE, JWT_AUTH_SECURE,
+    JWT_AUTH_COOKIE, JWT_AUTH_SAMESITE, JWT_AUTH_SECURE, JWT_AUTH_REFRESH_COOKIE,
 )
 
 
